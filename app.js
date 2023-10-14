e42e787866a87f039e9b38a25e4b0d6bd793615d
Szilard Hadobas <systeel@gmail.com>
2023-10-14 18:20:45 -0400

    personal homepage v1.0

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+const path = require('path');
+const nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'projectvera.requests@gmail.com',
+		pass: 'vegn fnds hcco pxcq'
+	}
+});
+
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+
+
+var PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname,'/static/web/index.html'));
+});
+
+app.get('/body', (req, res) => {
+	res.sendFile(path.join(__dirname,'/static/web/body.html'));
+});
+
+app.put('/uzenet', (req, res) => {
+	var mailOptions = {
+		from: req.body.tg,
+		to: 'systeel@gmail.com',
+		subject: '[HONLAP]:' + req.body.oj,
+		text: 'Email from ' + req.body.tg + ':\n' + req.body.mg
+	};
+	
+	transporter.sendMail(mailOptions, function(err, info) {
+		if (err) {
+			console.log(err)
+			res.json(err);
+		} else {
+			console.log('mail sent from ' + req.body.tg);
+			res.json({ok: 1});
+		}
+	});
+});
+
+app.listen(PORT,()=>{
+	console.log("app listening on port " + PORT);
+});
+
---
+++ static/src/ImageSlot.js
@@ -0,0 +1,51 @@
+class ImageSlot {
+	constructor(o) {
+		var self = this;
+		this.id = o.id;
+		this.pos = {x:0, y:0};
+		this.link = o.link;
+		this.img = (new Image());
+		this.width = o.width || -1;
+		this.height = o.height || -1;
+		this.scale = o.scale || 1;
+		this.baseWidth = o.baseWidth;
+		this.loaded = false;
+		this.img.onload = function(e){
+			if (self.width == -1) {
+				self.width = self.img.width;
+			}
+			if (self.height == -1) {
+				self.height = self.img.height;
+			}
+			self.loaded = true;
+		}
+		this.error = this.img.onerror = function(e){
+			self.loaded = false;
+		}
+	}
+
+	loadImage(link) {
+		if (link != undefined) {
+			this.link = link;
+		}
+		this.img.src = this.link;
+	}
+	
+	draw(ctx) {
+		if (this.loaded) {
+			ctx.drawImage(this.img, this.pos.x, this.pos.y, this.width*this.scale, this.height*this.scale);
+		}
+	}
+	
+	isHit(x, y) {
+		if ((y >= this.pos.y) && (y <= (this.pos.y + this.img.height * this.scale))) {
+			const bw = this.baseWidth * this.scale;
+			const w = this.width * this.scale;
+			const h = this.img.height * this.scale;
+			const dx = (h-y+this.pos.y)*(w-bw)/h;
+			if ((x >= dx + this.pos.x) && (x <= this.pos.x + bw + dx)) return true;
+		}
+		return false;
+	}
+	
+}
